1a9acadd130cf822a5d924b7a21d138c411271cd
xaawii <xavixumg@gmail.com>
2024-07-23 16:33:02 +0200

    last changes

--- user-service/src/main/java/com/xmartin/userservice/infraestructure/controller/UserController.java
+++ user-service/src/main/java/com/xmartin/userservice/infraestructure/controller/UserController.java
@@ -5,8 +5,12 @@ import com.xmartin.userservice.application.services.UserService;
 import com.xmartin.userservice.domain.exceptions.EmailAlreadyInUseException;
 import com.xmartin.userservice.domain.exceptions.UserNotFoundException;
 import com.xmartin.userservice.infraestructure.controller.dtos.UserRequest;
+import com.xmartin.userservice.infraestructure.controller.dtos.UserResponse;
 import com.xmartin.userservice.infraestructure.controller.mappers.UserMapper;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +29,10 @@ public class UserController {
 
 
     @Operation(summary = "Save or update", description = "Save or update a user in the application")
+    @ApiResponse(responseCode = "200", description = "Successfully saved user",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))})
+    @ApiResponse(responseCode = "401", description = "You are not authorized to access", content = @Content)
+    @ApiResponse(responseCode = "400", description = "Email already in use", content = @Content)
     @PostMapping("/save")
     public ResponseEntity<?> saveUser(@Valid @RequestBody UserRequest userRequest) throws EmailAlreadyInUseException {
 
@@ -33,6 +41,10 @@ public class UserController {
     }
 
     @Operation(summary = "Delete user by email", description = "Delete user by email")
+    @ApiResponse(responseCode = "200", description = "Successfully deleted user",
+            content = {@Content(mediaType = "text/plain", schema = @Schema(type = "String"))})
+    @ApiResponse(responseCode = "401", description = "You are not authorized to access", content = @Content)
+    @ApiResponse(responseCode = "400", description = "User Not Found", content = @Content)
     @DeleteMapping("/email/{email}")
 
     public ResponseEntity<?> deleteUser(@PathVariable String email) throws UserNotFoundException {
@@ -43,6 +55,10 @@ public class UserController {
     }
 
     @Operation(summary = "Get user by email", description = "Get user by email")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved user",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))})
+    @ApiResponse(responseCode = "401", description = "You are not authorized to access", content = @Content)
+    @ApiResponse(responseCode = "400", description = "User Not Found", content = @Content)
     @GetMapping("/email/{email}")
     public ResponseEntity<?> getUserByEmail(@PathVariable String email) throws UserNotFoundException {
 
@@ -51,6 +67,10 @@ public class UserController {
     }
 
     @Operation(summary = "Get user by id", description = "Get user by id")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved user",
+            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UserResponse.class))})
+    @ApiResponse(responseCode = "401", description = "You are not authorized to access", content = @Content)
+    @ApiResponse(responseCode = "400", description = "User Not Found", content = @Content)
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Integer id) throws UserNotFoundException {
 
@@ -58,7 +78,10 @@ public class UserController {
 
     }
 
-    @Operation(summary = "Check if user exist by email", description = "Check if user exist by email")
+    @Operation(summary = "Check if user exists by email", description = "Check if user exists by email")
+    @ApiResponse(responseCode = "200", description = "Successfully check if user exists",
+            content = {@Content(mediaType = "text/plain", schema = @Schema(type = "boolean"))})
+    @ApiResponse(responseCode = "401", description = "You are not authorized to access", content = @Content)
     @GetMapping("/exist/email/{email}")
     public ResponseEntity<?> getUserExistByEmail(@PathVariable String email) {
 
